92692e9ef598b8dde72868bfab7236892d654784
James Conkling <james.lane.conkling@gmail.com>
2025-02-03 09:36:23 -0800

    parameterize path and sequence filtering

--- README.md
+++ README.md
@@ -14,7 +14,8 @@ The script supports the following configurations via ENV variables:
 * CONCURRENCY - max number of concurrent queries [default: 18]
 * TIMEOUT - query timeout in ms. by default, no timeout is set [optional]
 * MIN_SUPPLY_CHAIN_SIZE - minimum size of supply chain. raising this number restricts queries to more expensive nodes [default: 2000]
-* QUERY_RESPONSE_TYPE - pass 'graph' to return the entire supply chain graph. pass 'count' to return only supply chain count [default: graph]
+* QUERY_DATE_FILTER - pass `path_date_filter` to filter dates by path. pass `segment_date_filter` to filter dates by pairwise segments [default: `path_date_filter`]
+* QUERY_RESPONSE_TYPE - pass `graph` to return the entire supply chain graph. pass `count` to return only supply chain count [default: `graph`]
 * USING_HOPS_LIMIT - limit the number of edges traversed using Memgraph's `USING HOPS LIMIT` directive. by default, directive is not included [optional]
 * QUERY_MEMORY_LIMIT - limit the query memory usage using Memgraph's `QUERY MEMORY LIMIT` directive. by default, no limit is set [optional]
 
--- index.ts
+++ index.ts
@@ -7,9 +7,15 @@ const TIME = process.env.TIME !== undefined ? parseInt(process.env.TIME, 10) : u
 const CONCURRENCY = process.env.CONCURRENCY !== undefined ? parseInt(process.env.CONCURRENCY, 10) : 18
 const TIMEOUT = process.env.TIMEOUT !== undefined ? parseInt(process.env.TIMEOUT) : undefined
 const MIN_SUPPLY_CHAIN_SIZE = process.env.MIN_SUPPLY_CHAIN_SIZE !== undefined ? parseInt(process.env.MIN_SUPPLY_CHAIN_SIZE, 10) : 2000
+const QUERY_DATE_FILTER = process.env.QUERY_DATE_FILTER ?? 'path_date_filter'
 const QUERY_RESPONSE_TYPE = process.env.QUERY_RESPONSE_TYPE ?? 'graph'
 const USING_HOPS_LIMIT = process.env.USING_HOPS_LIMIT !== undefined ? parseInt(process.env.USING_HOPS_LIMIT, 10) : undefined
 const QUERY_MEMORY_LIMIT = process.env.QUERY_MEMORY_LIMIT
+
+if (QUERY_DATE_FILTER !== 'path_date_filter' && QUERY_DATE_FILTER !== 'segment_date_filter') {
+  console.error(`Invalid QUERY_DATE_FILTER param ${QUERY_DATE_FILTER}. Expected values 'path_date_filter', 'segment_date_filter'.`)
+  process.exit(1)
+}
 
 if (QUERY_RESPONSE_TYPE !== 'graph' && QUERY_RESPONSE_TYPE !== 'count') {
   console.error(`Invalid QUERY_RESPONSE_TYPE param ${QUERY_RESPONSE_TYPE}. Expected values 'graph', 'map'.`)
@@ -32,49 +38,102 @@ let totalRequestCount = 0
 let successCount = 0
 let errorCount = 0
 const START_TIME = Date.now()
-const QUERY = (
-  USING_HOPS_LIMIT ? `USING HOPS LIMIT ${USING_HOPS_LIMIT} ` : ''
-) + `
-  MATCH (a:company { id: $id })<-[r1:ships_to]-(b)
-  WHERE b != a 
-  WITH a, b, collect(r1) AS r1
+let QUERY = USING_HOPS_LIMIT ? `USING HOPS LIMIT ${USING_HOPS_LIMIT} ` : ''
+
+if (QUERY_DATE_FILTER === 'path_date_filter') {
+  QUERY += `
+    MATCH (a:company { id: $id })<-[r1:ships_to]-(b)
+      WHERE b != a
+    WITH a, b, r1.hs_code AS component, r1.max_date AS max_date, r1.min_date AS min_date, r1
     OPTIONAL MATCH (b)<-[r2:ships_to]-(c)
-    WHERE c != b AND c != a AND any(export IN r1
-    WHERE (
-    export.min_date >= r2.min_date AND export.min_date <= r2.max_date AND
-    product_map.is_component(export.hs_code, r2.hs_code)
-    ))
-  WITH a, b, c, r1, collect(r2) AS r2
+      WHERE c != b AND c != a
+        AND product_map.is_component(component, r2.hs_code)
+        AND max_date >= r2.min_date
+        AND min_date <= r2.max_date
+    WITH a, b, c, r2.hs_code AS component,
+      CASE
+        WHEN max_date < r2.max_date THEN max_date ELSE r2.max_date
+      END AS max_date,
+      CASE 
+        WHEN min_date > r2.min_date THEN min_date ELSE r2.min_date 
+      END AS min_date,
+      r1, r2
     OPTIONAL MATCH (c)<-[r3:ships_to]-(d)
-    WHERE d != c AND d != b AND d != a AND any(export IN r2
-    WHERE (
-    export.min_date >= r3.min_date AND export.min_date <= r3.max_date AND
-    product_map.is_component(export.hs_code, r3.hs_code)
-    ))
-  WITH a, b, c, d, r1, r2, collect(r3) AS r3
+      WHERE d != c AND d != b AND d != a
+        AND product_map.is_component(component, r3.hs_code)
+        AND max_date >= r3.min_date
+        AND min_date <= r3.max_date
+    WITH a, b, c, d, r3.hs_code AS component,
+      CASE
+        WHEN max_date < r3.max_date THEN max_date ELSE r3.max_date
+      END AS max_date,
+      CASE 
+        WHEN min_date > r3.min_date THEN min_date ELSE r3.min_date 
+      END AS min_date,
+      r1, r2, r3
+    WITH a, b, c, d, component, max_date, min_date, collect(r1) + collect(r2) AS edges, r3
     OPTIONAL MATCH (d)<-[r4:ships_to]-(e)
-    WHERE e != d AND e != c AND e != b AND e != a AND any(export IN r3
-    WHERE (
-    export.min_date >= r4.min_date AND export.min_date <= r4.max_date AND
-    product_map.is_component(export.hs_code, r4.hs_code)
-    ))
-  WITH a, b, c, d, e, r1, r2, r3, collect(r4) AS r4
-` + (
-  QUERY_RESPONSE_TYPE === 'graph'
-    ? `
-      UNWIND [a, b, c, d, e] + r1 + r2 + r3 + r4 AS item
-      RETURN extract(
-        item IN collect(distinct item) |
-        CASE valueType(item)
-          WHEN 'NODE' THEN [ID(item), item.id, item.label, item.risk]
-          ELSE [ID(startNode(item)), ID(endNode(item)), item.hs_code_int, item.max_date, item.min_date, item.product_origin, item.shipment_arrival, item.shipment_departure]
-        END
-      ) AS items
-    `
-    : `RETURN count(*) AS count `
-) + (
-  QUERY_MEMORY_LIMIT ? `QUERY MEMORY LIMIT ${QUERY_MEMORY_LIMIT};` : ';'
-)
+      WHERE e != d AND e != c AND e != b AND e != a
+        AND product_map.is_component(component, r4.hs_code)
+        AND max_date >= r4.min_date
+        AND min_date <= r4.max_date
+    UNWIND edges + [r3, r4, a, b, c, d, e] AS item
+  `
+} else {
+  QUERY += `
+    MATCH (a:company { id: $id })<-[r1:ships_to]-(b)
+      WHERE b != a 
+    WITH a, b, collect(r1) AS r1
+    OPTIONAL MATCH (b)<-[r2:ships_to]-(c)
+      WHERE c != b AND c != a AND any(export IN r1
+      WHERE (
+      export.min_date >= r2.min_date AND export.min_date <= r2.max_date AND
+      product_map.is_component(export.hs_code, r2.hs_code)
+      ))
+    WITH a, b, c, r1, collect(r2) AS r2
+    OPTIONAL MATCH (c)<-[r3:ships_to]-(d)
+      WHERE d != c AND d != b AND d != a AND any(export IN r2
+      WHERE (
+      export.min_date >= r3.min_date AND export.min_date <= r3.max_date AND
+      product_map.is_component(export.hs_code, r3.hs_code)
+      ))
+    WITH a, b, c, d, r1, r2, collect(r3) AS r3
+    OPTIONAL MATCH (d)<-[r4:ships_to]-(e)
+      WHERE e != d AND e != c AND e != b AND e != a AND any(export IN r3
+      WHERE (
+      export.min_date >= r4.min_date AND export.min_date <= r4.max_date AND
+      product_map.is_component(export.hs_code, r4.hs_code)
+      ))
+    WITH a, b, c, d, e, r1, r2, r3, collect(r4) AS r4
+    UNWIND [a, b, c, d, e] + r1 + r2 + r3 + r4 AS item
+  `
+}
+
+if (QUERY_RESPONSE_TYPE === 'graph') {
+  QUERY += `
+    WITH item WHERE item IS NOT NULL
+    RETURN extract(
+      item IN collect(distinct item) |
+      CASE valueType(item)
+        WHEN 'NODE' THEN [ID(item), item.id, item.label, item.risk]
+        ELSE [ID(startNode(item)), ID(endNode(item)), item.hs_code_int, item.max_date, item.min_date, item.product_origin, item.shipment_arrival, item.shipment_departure]
+      END
+    ) AS items
+  `
+} else {
+  QUERY += `
+    WITH item WHERE item IS NOT NULL
+    WITH collect(distinct item) AS items
+    WITH collect(distinct item) AS items
+    RETURN size(items) AS count
+  `
+}
+
+if (QUERY_MEMORY_LIMIT) {
+  QUERY += `QUERY MEMORY LIMIT ${QUERY_MEMORY_LIMIT};`
+}
+
+
 
 const queryRunner = async () => {
   while (true) {
